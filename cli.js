fd72e36e3c848a8a34d4b2e016d415cac01009ee
belkisch <belkisch@gmail.com>
2022-01-15 11:56:53 -0500

    1erRespaldo

---
+++ cli.js
@@ -0,0 +1,16 @@
+// const opcionUno =
+let nroArgs = process.argv.length;
+let ruta = process.argv[1];
+
+console.log(ruta);
+
+if(nroArgs > 0 && nroArgs < 4) {
+/*
+  switch(nroArgs) {
+    case 1:
+  }
+
+*/
+}else{
+  console.log("Cantidad de Parametros Incorrectos");
+}
--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+const path = require('path');
+const fs = require('fs');
+
+// 1.- Validar que la Ruta Existe
+let validarRuta = (dir) => fs.existsSync(dir);
+
+// 2.- Validar que es Absolutar
+let validarAbsoluta = function(dir) {
+  if(path.isAbsolute(dir)) {
+    return dir
+  }
+  return path.resolve(dir)
+}
+
+// 3.- Validar si es Directorio
+let validarDirectorio = (dir) => fs.lstatSync(dir).isDirectory();
+
+// 4.- Recorrer Directorio y Obtener los Archivos .md
+const recorrerDirectorio = (dir) => {
+  let archivosMD = [];
+  if(validarDirectorio(dir)) {
+    const directorio = fs.readdirSync(dir);
+    directorio.forEach((file) => {
+      archivosMD = archivosMD.concat(recorrerDirectorio(path.join(dir, file)));
+    });
+  }else{
+    if(path.extname(dir) === '.md'){
+      archivosMD.push(dir);
+    }
+  }
+  return archivosMD;
+};
+
+// 5.- Recorrer el Arreglo de archivosMD y obtener los links
+
+
+
+
+console.log(recorrerDirectorio('prueba'))
+
+
+
+/*
 module.exports = () => {
   // ...
 };
+ */
